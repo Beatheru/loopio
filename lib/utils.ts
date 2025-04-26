fe46f7a46ae5d492efcb1c6e8672b095e3fb25a2
Andrew <beathandrewc@gmail.com>
2025-04-26 18:16:24 -0400

    Fix formatSeconds to display seconds cleaner if it is a long decimal

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,7 +14,9 @@ export function formatSeconds(input: number) {
   const hours = Math.floor(input / 3600);
   input = input % 3600;
   const minutes = Math.floor(input / 60);
-  const seconds = input % 60;
+  const [seconds, secondDecimals] = String(Number(input % 60).toFixed(2)).split(
+    ".",
+  );
 
-  return `${hours > 0 ? `${hours}:` : ""}${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  return `${hours > 0 ? `${hours}:` : ""}${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}.${secondDecimals}`;
 }
